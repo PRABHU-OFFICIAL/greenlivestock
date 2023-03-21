3cdb7334c199cdafdb4e4fb63304d4f49eafd8ae
PRABHU PRASAD PENTHOI <72146816+PRABHU-OFFICIAL@users.noreply.github.com>
2023-03-21 20:37:42 +0530

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:greenlivestock/homePage.dart';
 import 'package:greenlivestock/idealsPage.dart';
